b9fb81cbc8ecbd9f873e2202d46beeb86cdc1478
Guilherme Cabrera <M306833@one.merckgroup.com>
2021-08-14 17:01:26 -0300

    New git ignores added

---
+++ s1/s1PrimeiroProjeto/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace s1PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Tecle enter para finalizar");
+            Console.ReadLine();
+        }
+    }
+}
