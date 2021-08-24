91d46d059e4cafddf0251c3212e34d0beab444b5
G Cabrera <guilherme.cabrera@edu.ung.br>
2021-08-24 02:17:07 -0300

    aula de condicionais

---
+++ s4Conditionals/s4Conditionals/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto de condicionais");
+
+
+            //estrutura condicional if
+            int idade = 15;
+            string name = "João";
+
+            if(idade>=18)
+            {
+                Console.WriteLine(name + " é maior de idade\nIdade: "+idade);
+            } else
+            {
+                Console.WriteLine(name + " é menor de idade \nIdade:"+idade);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
