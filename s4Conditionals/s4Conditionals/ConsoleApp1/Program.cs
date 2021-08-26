09be4b18ad7a517605b31322513edf294bda0d5f
G Cabrera <guilherme.cabrera@edu.ung.br>
2021-08-26 01:30:33 -0300

    conditionals 2 and 3

--- s4Conditionals/s4Conditionals/ConsoleApp1/Program.cs
+++ s4Conditionals/s4Conditionals/ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
             Console.WriteLine("Projeto de condicionais");
 
 
-            //estrutura condicional if
+            //estrutura condicional if A1
             int idade = 15;
             string name = "João";
 
@@ -20,6 +20,26 @@ namespace ConsoleApp1
             {
                 Console.WriteLine(name + " é menor de idade \nIdade:"+idade);
             }
+
+            //Estruturas condicionais 2 A2 || --> e or ; && --> ou or
+            if(idade>14 && name == "João")
+            {
+                Console.WriteLine("Have 15 Years and your name is João");
+            }
+
+            if(idade>14 && idade == 15)
+            {
+                Console.WriteLine("Have 15 Years old");
+            }
+
+            bool verification = idade < 16;
+            if (verification)
+            {
+                Console.WriteLine("Verification is true");
+            }
+
+            //scope of var's
+
 
             Console.ReadLine();
         }
