9821d7455c6033a8cd669be43018d083c0c12822
G Cabrera <guilherme.cabrera@edu.ung.br>
2021-09-07 13:30:50 -0300

    class as criteria in function

--- CSharp 2 - OO/s1s2s3PrimeiraClasseOO/ConsoleApp1/Program.cs
+++ CSharp 2 - OO/s1s2s3PrimeiraClasseOO/ConsoleApp1/Program.cs
@@ -9,22 +9,30 @@ namespace ConsoleApp1
 
             //Estrutura de classe --> object has been created on other file (contaCorrente.cs)
             bankAccount acc1 = new bankAccount();
-            
+            bankAccount acc2 = new bankAccount();
+
             //values to properties on class
             acc1.baAgency = 155;
-            acc1.baBalance = 5000;
             acc1.baName = "Guilherme Provencio";
             acc1.baNumber = 59667;
 
+            //values to properties on class
+            acc2.baAgency = 130;
+            acc2.baName = "José Frovencio";
+            acc2.baNumber = 58557;
 
-            Console.WriteLine("-------------------------------------------------------------------------");
+            Console.WriteLine("---------------------------------------------------------------------------------------------------------");
             Console.WriteLine("Agency: " + acc1.baAgency);
             Console.WriteLine("Number: " + acc1.baNumber);
-            acc1.withdraw(500);
-            Console.WriteLine("Balance: " + acc1.baBalance);
+            //acc1.withdraw(500);
+            Console.WriteLine("Balance: " + acc1.extrato());
             Console.WriteLine("Name: " + acc1.baName);
-            Console.WriteLine("-------------------------------------------------------------------------");
-
+            Console.WriteLine("---------------------------------------------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Transfer");
+            Console.WriteLine("---------------------------------------------------------------------------------------------------------");
+            acc1.transfer(250, acc2);
+            Console.WriteLine("---------------------------------------------------------------------------------------------------------");
             //when we are using objects, we have the difference of object values (when compared objects to objects) because each object has a reference number
             Console.ReadLine();
         }
--- CSharp 2 - OO/s1s2s3PrimeiraClasseOO/ConsoleApp1/contaCorrente.cs
+++ CSharp 2 - OO/s1s2s3PrimeiraClasseOO/ConsoleApp1/contaCorrente.cs
@@ -5,20 +5,44 @@ public class bankAccount
     public string baName;
     public int baNumber;
     public int baAgency;
-    public double baLimit;
+    private double baLimit = 1000;
     //here when you create a new bankAccount the account will have 500$
-    public double baBalance = 500;
+    private double baBalance = 500;
+
+
 
     //declaring function in C#
-    public void withdraw(double valueWithdraw)
+    public bool withdraw(double valueWithdraw)
     {
         if ((this.baLimit + this.baBalance) < valueWithdraw)
         {
             Console.WriteLine("Insuficient Balance and limit to procede with this operation, please check your limit and try again");
-        } else
-        {
-            this.baBalance -= valueWithdraw;
-            Console.WriteLine("Amount withdrawn successfully");
+            return false;
         }
+        //when we are using a non void function we dont need put an else in the end of the if because the return ends the function
+        this.baBalance -= valueWithdraw;
+        Console.WriteLine("Operation done successfully");
+        return true;
+    }
+    
+    public void deposit(double valueDeposit)
+    {
+        this.baBalance += valueDeposit;
+    }
+
+    public bool transfer(double valueTransfer, bankAccount newAcc)
+    {
+        if(!this.withdraw(valueTransfer))
+        {
+            return false;
+        }
+        
+        newAcc.baBalance += valueTransfer;
+        return true;
+    }
+
+    public double extrato()
+    {
+        return this.baBalance;
     }
 }
